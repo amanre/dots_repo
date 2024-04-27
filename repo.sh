1c7d34c1b01576d2bc50a7922af817ad855b4581
amanre <amanre556@gmail.com>
2024-04-27 22:23:19 +0200

    update

--- repo.sh
+++ repo.sh
@@ -7,7 +7,7 @@ echo "repo-add"
 repo-add -n -R -v  custom_repo.db.tar.gz *.pkg.tar.zst
 rm -v custom_repo.db
 rm -v custom_repo.files
-mv -v custom_repo.db.tar.gz nemesis_repo.db
+mv -v custom_repo.db.tar.gz custom_repo.db
 mv -v custom_repo.files.tar.gz custom_repo.files
 
 cd ..
