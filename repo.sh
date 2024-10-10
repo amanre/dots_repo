47e0686944a55e23c87ba0ba33ebbdfb58637caa
amanre <amanre556@gmail.com>
2024-10-10 04:20:03 +0200

    update

--- repo.sh
+++ repo.sh
@@ -8,7 +8,7 @@ repo-add -n -R -v  dots_repo.db.tar.gz *.pkg.tar.zst
 rm -v dots_repo.db
 rm -v dots_repo.files
 mv -v dots_repo.db.tar.gz dots_repo.db
-mv -v dotsrepo.files.tar.gz dots_repo.files
+mv -v dots_repo.files.tar.gz dots_repo.files
 
 cd ..
 echo "####################################"
