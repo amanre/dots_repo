291469d0fcb81f1818cc20e523ab7a34a2850c2d
amanre <amanre556@gmail.com>
2024-10-10 03:20:33 +0200

    update

--- repo.sh
+++ repo.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 cd x86_64
-rm amoslinux_repo*
+rm dots_repo*
 
 echo "repo-add"
-repo-add -n -R -v  amoslinux_repo.db.tar.gz *.pkg.tar.zst
-rm -v amoslinux_repo.db
-rm -v amoslinux_repo.files
-mv -v amoslinux_repo.db.tar.gz amoslinux_repo.db
-mv -v amoslinux_repo.files.tar.gz amoslinux_repo.files
+repo-add -n -R -v  dots_repo.db.tar.gz *.pkg.tar.zst
+rm -v dots_repo.db
+rm -v dots_repo.files
+mv -v dots_repo.db.tar.gz dots_repo.db
+mv -v dotsrepo.files.tar.gz dots_repo.files
 
 cd ..
 echo "####################################"
--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm amoslinux_repo*
+rm dots_repo*
 
 echo "repo-add"
-repo-add -n -R amoslinux_repo.db.tar.gz *.pkg.tar.zst
+repo-add -n -R dots_repo.db.tar.gz *.pkg.tar.zst
 sleep 5
 
 echo "####################################"
